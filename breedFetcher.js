f89a211fbe85db51ae2aebfad226bad73f9f41d2
JOON LEE <110060709+greenlemonkp@users.noreply.github.com>
2022-10-24 13:02:59 -0400

    test and ready to go final product

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,20 @@
 const request = require ('request');
-const searchWord = process.argv[2];
 const fs = require('fs');
 
+const fetchBreedDescription = (searchWord, callback) => {
 request(`https://api.thecatapi.com/v1/breeds/search?q=${searchWord}`, (error, response, body) => {
   if (error) {
-    console.log("Error", error);
-    return;
-  }
+    return callback(error, null)
+  } 
 const data = JSON.parse(body);
 let breed = data[0];
 if (breed) {
-  console.log(breed.description);
+  callback(null, breed.description);
 }
 else {
-  console.log('Breed not found!')
+  callback('Breed not found!', null)
 }
 });
+}
 
+module.exports = { fetchBreedDescription };
